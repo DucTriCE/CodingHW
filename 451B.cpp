98fef349361f3750231b9b76743706c75b44e5a1
DucTriCE <trild.ce@gmail.com>
2023-06-18 14:56:42 +0700

    Fix problem 451B 18/6

--- 451B.cpp
+++ 451B.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main (){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    int n, check=0;
-    vector<int> v, v1, vans(2);
+    int n;
+    cin >> n;
+    int start=-1, end=-1;
+    vector<int> v, v1;
     for(int i=0; i<n; i++){
         int x;
         cin >> x;
@@ -16,15 +18,25 @@ int main (){
     }
     sort(v1.begin(), v1.end());
     for(int i=0; i<n; i++){
-        if(v1[i]!=v[i]){
-            check++;
-            if(check<=2)vans.push_back(i+1);
-            else{
-                cout << "no";
-                return 0;
-            } 
+        if(v[i]!=v1[i]){
+            start=i;
+            break;
         }
     }
-    cout << "yes" << endl;
-    cout << vans[0]<< " " << vans[1];
+    for(int i=n-1; i>=0; i--){
+        if(v[i]!=v1[i]){
+            end=i;
+            break;
+        }
+    }
+    if(start==end)cout << "yes" << endl << "1 1";
+    else{
+        for(int i=end; i>=start; i--){
+            if(v[i]!=v1[end-i+start]){
+                cout << "no";
+                return 0;
+            }
+        }
+        cout << "yes" << endl << start+1 << " " << end+1;
+    }
 }
