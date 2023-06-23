809f63c9bb49ffedbaeea21c7c0eaf688410f072
DucTriCE <trild.ce@gmail.com>
2023-06-23 16:09:22 +0700

    Add problem 334B 23/6

--- 334B.cpp
+++ 334B.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 int main (){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    int n;
-    cin >> n;
+    set<int> stx, sty;
+    set<pair<int,int>> stp;
     vector<pair<int,int>> v;
-    for(int i=0; i<n; i++){
+    for(int i=0; i<8; i++){
         int x,y;
         cin >> x >> y;
+        stx.insert(x);
+        sty.insert(y);
+        stp.insert(make_pair(x,y));
         v.push_back(make_pair(x,y));
+    }
+    if(stx.size()!=3||sty.size()!=3||stp.size()!=8){
+        cout << "ugly";
+    }else{
+        set<int>::iterator it = stx.begin();
+        set<int>::iterator it2 = sty.begin();
+        int x2=*++it, y2=*++it2;
+        for(int i=0; i<8; i++){
+            if(v[i].first==x2 && v[i].second==y2){
+                cout << "ugly";
+                return 0;
+            }
+        }
+        cout << "respectable";
     }
 }
