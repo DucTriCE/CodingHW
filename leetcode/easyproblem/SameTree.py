7dcae310f27494a3f9967ace7805b6a4d4b3f441
DucTriCE <trild.ce@gmail.com>
2024-04-10 09:40:36 +0700

    Solved 10/4

---
+++ leetcode/easyproblem/SameTree.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSameTree(p, q) -> bool:
+    if p is None and q is None:
+        return True
+    if p is not None and q is not None:
+        return (p.val==q.val) and (isSameTree(p.left, q.left)) and (isSameTree(p.right, q.right))
+    return False
+
+if __name__ == '__main__':
+    root1 = Node(1)
+    root2 = Node(1)
+    root1.left = Node(2)
+    root1.right = Node(3)
+    root1.left.left = Node(4)
+    root1.left.right = Node(5)
+    
+    root2.left = Node(2)
+    root2.right = Node(3)
+    root2.left.left = Node(4)
+    root2.left.right = Node(5)
+
+    print(isSameTree(root1, root2))
