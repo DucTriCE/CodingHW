067c86a90902ae6721efd974108d7610499d2d3a
DucTriCE <trild.ce@gmail.com>
2024-07-08 23:53:46 +0700

    chore: delete createfile

--- leetcode/30daychallenge/createfile.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <file_name_without_py_extension>"
-    exit 1
-fi
-FILE_NAME=$1
-
-echo "if __name__ == '__main__':" >> "${FILE_NAME}.py"
-echo "    # Add your code here" >> "${FILE_NAME}.py"
